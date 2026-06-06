ed061622b19e89f5831b083368719075f5dcbc76
domenicofrancescomenga-code <61563877+Domenico-Francesco-Menga@users.noreply.github.com>
2026-06-07 00:02:58 +0200

    new commit

--- Ciao.py
+++ Ciao.py
@@ -1,12 +1,20 @@
 import pandas as pd
+import numpy as np
 
 data = {
     'Name': ['Alice', 'Bob', 'Charlie', 'Diana'],
     'Age': [25, 30, 35, 28],
-    'City': ['New York', 'London', 'Paris', 'Berlin'],
+    'City': ['New York', 'London', np.nan, 'London'],
     'Occupation': ['Engineer', 'Designer', 'Artist', 'Doctor']
 }
 
 df = pd.DataFrame(data)
+df['City'] = df['City'].fillna('Unknown')
+
+Names = df['Name']
 
 print(df)
+print(Names)
+
+grouped = df.groupby(['City','Occupation'])['Age'].sum()
+print(grouped)
